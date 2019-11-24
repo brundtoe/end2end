c8347d97cfeb249e78446a67ff77a4106791fa4b
Bent <bruntoe@outlook.dk>
2019-11-24 11:19:57 +0100

    WebdriverIO navigation på jstraining home page

---
+++ webdriverio-samples/test/jstraining.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+describe('Jstraining pages',  () => {
+    it('should have the title Training Samples', async () => {
+        await browser.url('http://localhost:3000/index.html');
+        const title = await browser.getTitle();
+        assert.strictEqual(title, 'Training Samples')
+    });
+
+    it('should have the Link text Promise', () => {
+        browser.url('http://localhost:3000/index.html');
+        const link = $("#promise");
+        console.log('Der er noget galt')
+        //console.log(link.getText());
+        assert.strictEqual(link.getText(),'Promise')
+        link.click()
+        browser.url('http://localhost:3000/promise/PromiseCustomer.html')
+        const title = browser.getTitle()
+        assert.strictEqual(title, 'Get Customer JSON')
+    })
+
+})
