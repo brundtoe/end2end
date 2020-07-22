60c530a28b8591f67963d1891dce587cbca55121
Jackie <brundtoe@outlook.dk>
2020-07-22 12:08:01 +0200

    skiftet protactor konfig til chrome og headless. direkte adgang til browseren

--- protractor-samples/conf.js
+++ protractor-samples/conf.js
@@ -1,11 +1,11 @@
 exports.config = {
-    //directConnect: true,
+    directConnect: true,
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./test/*spec.js'],
     capabilities: {
-        browserName: 'firefox',
-        'moz:firefoxOptions':{
+        browserName: 'chrome',
+        'chromeOptions':{
             args: ["--headless"]
         }
     }
